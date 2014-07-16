f0c459a41d0525ed62fc0ca7b4507be2bea11251
nishimoto <b111090@cc.it-hiroshima.ac.jp>
2014-07-16 09:55:59 +0900

    三澤さんのmigrateのミスを修正
    pullのミスじゃなくてmigrateのミスだったぽいです。

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -7,14 +7,10 @@ return array(
 			'default' => 
 			array(
 				0 => '001_create_authorities',
-				1 => '001_create_highschools',
-				2 => '002_create_systems',
-				3 => '002_create_teams',
-				4 => '003_create_highschools',
-				5 => '003_create_records',
-				6 => '004_create_systems',
-				7 => '004_create_teams',
-				8 => '005_create_records',
+				1 => '002_create_systems',
+				2 => '003_create_highschools',
+				3 => '004_create_teams',
+				4 => '005_create_records',
 			),
 		),
 		'module' => 
