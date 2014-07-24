e0bfd189d033a381aaf434410bc8619c89e538de
nishimoto <b111090@cc.it-hiroshima.ac.jp>
2014-07-24 19:14:36 +0900

    デフォルトルートをログインページにしました

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,6 +1,7 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
+//	'_root_'  => 'welcome/index',  // The default route
+	'_root_'  => 'auth/login',
 	'_404_'   => 'welcome/404',    // The main 404 route
 	
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
